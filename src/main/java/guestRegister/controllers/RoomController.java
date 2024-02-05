d7815b768f0963a975f049e42b3499c190aae813
DanielLorenco <danlor@seznam.cz>
2024-02-05 15:11:18 +0100

    added RoomRepository and RoomService classes, added addRoom method into service and controller

--- src/main/java/guestRegister/controllers/RoomController.java
+++ src/main/java/guestRegister/controllers/RoomController.java
@@ -1,6 +1,9 @@
 package guestRegister.controllers;
 
 import guestRegister.dto.RoomDTO;
+import guestRegister.dto.mapper.RoomMapper;
+import guestRegister.entity.RoomEntity;
+import guestRegister.service.RoomService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class RoomController {
 
+    private final RoomService roomService;
+
+    private final RoomMapper roomMapper;
+
+    public RoomController(RoomService roomService, RoomMapper roomMapper) {
+        this.roomService = roomService;
+        this.roomMapper = roomMapper;
+    }
+
 @PostMapping({"/room", "/room/"})
     public RoomDTO addRoom(@RequestBody RoomDTO roomDTO) {
-    System.out.println("TO DO...");
-    return  new RoomDTO();
+    return  roomService.addRoom(roomDTO);
 }
+
+
 }
---
+++ src/main/java/guestRegister/service/RoomService.java
@@ -0,0 +1,23 @@
+package guestRegister.service;
+
+
+import guestRegister.dto.RoomDTO;
+import guestRegister.dto.mapper.RoomMapper;
+import guestRegister.entity.RoomEntity;
+import guestRegister.entity.repository.GuestRepository;
+import guestRegister.entity.repository.RoomRepository;
+
+public class RoomService {
+
+    private RoomRepository roomRepository;
+
+    private GuestRepository guestRepository;
+
+    private RoomMapper roomMapper;
+
+    public RoomDTO addRoom(RoomDTO roomDTO) {
+        RoomEntity room = roomMapper.toEntity(roomDTO);
+        RoomEntity saved = roomRepository.save(room);
+        return roomMapper.toDTO(saved);
+    }
+}
